b3d95e4db3f7925ba0a786ca5807669a5a4b30e8
Pablo Bejarano <pablobejarano2112@gmail.com>
2024-04-12 22:02:52 -0300

    add(ejercicio 2)

---
+++ src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,30 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombrePais;
+		int edadPersona;
+		int alturaEdificio;
+		double precioProducto;
+		long numTelefono;
+		double calculoCos;
+		
+		nombrePais = "Argentina";
+		edadPersona = 22;
+		alturaEdificio = 90;
+		precioProducto = 89.99;
+		numTelefono = 15008099;
+		calculoCos = 0.8776;
+		
+		System.out.println("Pais: "+ nombrePais);
+		System.out.println("Edad: "+ edadPersona);
+		System.out.println("Altura de un edificio: "+ alturaEdificio);
+		System.out.println("Precio de un producto: "+ precioProducto);
+		System.out.println("Telefono: "+ numTelefono);
+		System.out.println("Coseno de 0.5: "+ calculoCos);
+		
+	}
+
+}
