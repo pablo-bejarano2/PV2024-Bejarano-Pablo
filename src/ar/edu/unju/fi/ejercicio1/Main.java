00422aa2779014f69bb22b5780d040ef0a87a7c6
Pablo Bejarano <pablobejarano2112@gmail.com>
2024-04-12 21:34:05 -0300

    add(ejercicio 1)

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n1 = 8;
+		int n2 = 5;
+		int n3 = 1;
+		int n4 = 20;
+		int n5 = 9;
+		double suma;
+		double promedio;
+		suma = (n1+n2+n3+n4+n5);
+		promedio = suma/5;
+		System.out.println(promedio);
+	}
+
+}
